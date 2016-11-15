61e4fd1ad3c918f8ada75f2208ccb523d2105b8f
cody brubaker and haley swain <haley@>
2016-11-15 15:23:17 -0800

    add some specs kind of work

--- lib/numberword.rb
+++ lib/numberword.rb
@@ -11,7 +11,7 @@ class Fixnum
   number.store(3,"three")
   number.store(4,"four")
   number.store(5,"five")
-  number.store(6,"six")
+  number.store(6," six")
   number.store(7,"seven")
   number.store(8,"eight")
   number.store(9,"nine")
@@ -35,11 +35,11 @@ class Fixnum
   number.store(90,"ninety")
   number.store(100,"one hundred")
   number.store(200,"two hundred")
-  number.store(300,"three hundred")
+  number.store(300,"three hundred ")
   number.store(400,"four hundred")
   number.store(500,"five hundred")
   number.store(600,"six hundred")
-  number.store(700,"seven hundred")
+  number.store(700,"seven hundred ")
   number.store(800,"eight hundred")
   number.store(900,"nine hundred")
   number.store(1000,"one thousand")
@@ -52,55 +52,123 @@ class Fixnum
   number.store(8000,"eight thousand")
   number.store(9000,"nine thousand")
   number.store(10000,"ten thousand")
-  # number.store(11000,"eleven thousand")
-  # number.store(12000,"twelve thousand")
-  # number.store(13000,"thirteen thousand")
-  # number.store(14000,"fourteen thousand")
-  # number.store(15000,"fifteen thousand")
-  # number.store(16000,"sixteen thousand")
-  # number.store(17000,"seventeen thousand")
-  # number.store(18000,"eighteen thousand")
-  # number.store(19000,"nineteen thousand")
+
   define_method(:numberword) do
 
   digit = self.to_s.split('').map(&:to_i)
   empty_array = []
+  # 1 digit number
   if digit.length() === 1
     ones_spot = number.fetch(digit.at(0))
     empty_array.push(ones_spot)
     empty_array.join()
-    empty_array = empty_array.map { |i| "" + i.to_s + ""}.join("")
+    output = empty_array.map { |i| "" + i.to_s + ""}.join("")
+  # 2 digit number - tens
   elsif digit.length() === 2
+    if (digit.at(0) === 1)
+      empty_array = empty_array.push(digit.at(0), digit.at(1)).join().to_i()
+      output = number.fetch(empty_array)
+    else
     tens_spot = number.fetch(digit.at(0) * 10)
     ones_spot = number.fetch(digit.at(1))
     empty_array.push(tens_spot, ones_spot)
     empty_array.join()
-    empty_array = empty_array.map { |i| "" + i.to_s + ""}.join("")
+    output = empty_array.map { |i| "" + i.to_s + ""}.join("")
+  end
+  # 3 digit number - hundreds
   elsif digit.length() === 3
+    if digit.at(1) === 1
+      hundreds_spot = number.fetch(digit.at(0) * 100)
+      empty_array = empty_array.push(digit.at(1), digit.at(2)).join().to_i()
+      last_two = number.fetch(empty_array.at(1))
+      output = empty_array.push(hundreds_spot,last_two)
+      output.join()
+      output = output.map { |i| "" + i.to_s + ""}.join("")
+    elsif
     hundreds_spot = number.fetch(digit.at(0) * 100)
     tens_spot = number.fetch(digit.at(1) * 10)
     ones_spot = number.fetch(digit.at(2))
     empty_array.push(hundreds_spot,tens_spot, ones_spot)
     empty_array.join()
-    empty_array = empty_array.map { |i| "" + i.to_s + ""}.join("")
+    output = empty_array.map { |i| "" + i.to_s + ""}.join("")
+    end
+  # 4 digit number - thousands
   elsif digit.length() === 4
+    if digit.at(2) === 1
+      thousands_spot = number.fetch(digit.at(0) * 1000)
+      hundreds_spot = number.fetch(digit.at(1) * 100)
+      empty_array = empty_array.push(digit.at(2), digit.at(3)).join().to_i()
+      last_two = number.fetch(empty_array(2))
+      output = empty_array.push(thousands_spot,hundreds_spot,last_two)
+      output.join()
+      output = output.map { |i| "" + i.to_s + ""}.join("")
+    elsif
     thousands_spot = number.fetch(digit.at(0) * 1000)
     hundreds_spot = number.fetch(digit.at(1) * 100)
     tens_spot = number.fetch(digit.at(2) * 10)
     ones_spot = number.fetch(digit.at(3))
     empty_array.push(thousands_spot,hundreds_spot,tens_spot,ones_spot)
     empty_array.join()
-    empty_array = empty_array.map { |i| "" + i.to_s + ""}.join("")
+    output = empty_array.map { |i| "" + i.to_s + ""}.join("")
+    end
+  # 5 digit number - ten thousands
   elsif digit.length() === 5
-    ten_thousands_spot = number.fetch(digit.at(0) * 10000)
-    thousands_spot = number.fetch(digit.at(1) * 1000)
+    tens_thousands_spot = number.fetch(digit.at(0) * 10)
+    thousands_spot = number.fetch(digit.at(1)) + " thousand"
     hundreds_spot = number.fetch(digit.at(2) * 100)
     tens_spot = number.fetch(digit.at(3) * 10)
     ones_spot = number.fetch(digit.at(4))
-    empty_array.push(ten_thousands_spot,hundreds_spot,tens_spot,ones_spot)
+    empty_array.push(tens_thousands_spot,thousands_spot,hundreds_spot,tens_spot,ones_spot)
     empty_array.join()
-    empty_array = empty_array.map { |i| "" + i.to_s + ""}.join("")
+    output = empty_array.map { |i| "" + i.to_s + ""}.join("")
+  # 6 digit number - hundred thosands
+  elsif digit.length() === 6
+    hundred_thousands_spot = number.fetch(digit.at(0) * 100)
+    tens_thousands_spot = number.fetch(digit.at(1) * 10)
+    thousands_spot = number.fetch(digit.at(2)) + " thousand "
+    hundreds_spot = number.fetch(digit.at(3) * 100)
+    tens_spot = number.fetch(digit.at(4) * 10)
+    ones_spot = number.fetch(digit.at(5))
+    empty_array.push(hundred_thousands_spot,tens_thousands_spot,thousands_spot,hundreds_spot,tens_spot,ones_spot)
+    empty_array.join()
+    output = empty_array.map { |i| "" + i.to_s + ""}.join("")
+  # 7 digit number - millions
+  elsif digit.length() === 7
+    million_spot = number.fetch(digit.at(0)) + " million "
+    hundred_thousands_spot = number.fetch(digit.at(1) * 100)
+    tens_thousands_spot = number.fetch(digit.at(2) * 10)
+    thousands_spot = number.fetch(digit.at(3))
+    if hundred_thousands_spot + tens_thousands_spot + thousands_spot != ""
+      thousands_spot = number.fetch(digit.at(3)) + " thousand "
     end
-    empty_array
+    hundreds_spot = number.fetch(digit.at(4) * 100)
+    tens_spot = number.fetch(digit.at(5) * 10)
+    ones_spot = number.fetch(digit.at(6))
+    empty_array.push(million_spot,hundred_thousands_spot,tens_thousands_spot,thousands_spot,hundreds_spot,tens_spot,ones_spot)
+    empty_array.join()
+    output = empty_array.map { |i| "" + i.to_s + ""}.join("")
+  # 8 digit number - ten millions
+  # elsif digit.length() === 8
+  #   ten_million_spot = number.fetch(digit.at(0))
+  #   million_spot = number.fetch(digit.at(1)) + " million "
+  #   hundred_thousands_spot = number.fetch(digit.at(2) * 100)
+  #   tens_thousands_spot = number.fetch(digit.at(3) * 10)
+  #   thousands_spot = number.fetch(digit.at(4))
+  #   if hundred_thousands_spot + tens_thousands_spot + thousands_spot != ""
+  #     thousands_spot = number.fetch(digit.at(4)) + " thousand "
+  #   end
+  #   hundreds_spot = number.fetch(digit.at(5) * 100)
+  #   tens_spot = number.fetch(digit.at(6) * 10)
+  #   if digit.at(6) === 1
+  #     last_two = empty_array.push(digit.at(6), digit.at(7)).join().to_i()
+  #     last_two = number.fetch(last_two)
+  #   end
+  #   ones_spot = number.fetch(digit.at(7))
+  #   empty_array.push(ten_million_spot,million_spot,hundred_thousands_spot,tens_thousands_spot,thousands_spot,hundreds_spot,tens_spot,ones_spot)
+  #   empty_array.join()
+  #   output = empty_array.map { |i| "" + i.to_s + ""}.join("")
+    end
+
+    output
   end
 end
--- spec/numberword_spec.rb
+++ spec/numberword_spec.rb
@@ -6,18 +6,27 @@ describe("Fixnum#numberword") do
     expect(1.numberword()).to(eq("one"))
   end
   it ("returns the word form of a double digit number up to 19") do
-    expect(10.numberword()).to(eq("ten"))
+    expect(13.numberword()).to(eq("thirteen"))
   end
   it ("returns the word form of a number between 20 and 99") do
     expect(25.numberword()).to(eq("twentyfive"))
   end
   it ("returns the word form of a triple digit number") do
-    expect(100.numberword()).to(eq("one hundred"))
+    expect(113.numberword()).to(eq("one hundred thirteen"))
   end
   it ("returns the word form of a quadruple digit number") do
-    expect(2000.numberword()).to(eq("two thousand"))
+    expect(2011.numberword()).to(eq("two thousand eleven"))
   end
   it ("returns the word form of a five digit number") do
     expect(30000.numberword()).to(eq("thirty thousand"))
   end
+  it ("returns the word form of a six digit number") do
+    expect(356750.numberword()).to(eq("three hundred fifty six thousand seven hundred fifty"))
+  end
+  it ("returns the word form of a seven digit number") do
+    expect(1350200.numberword()).to(eq("one million three hundred fifty thousand two hundred"))
+  end
+  # it ("returns the word form of an eight digit number") do
+  #   expect(13450213.numberword()).to(eq("one million three hundred forty five thousand two hundred thirteen"))
+  # end
 end
